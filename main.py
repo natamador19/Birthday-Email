354842e5fcda1d3832ae9f54b3d9022259fc238d
Nataly Amador <73267858+natamador19@users.noreply.github.com>
2025-06-23 15:04:51 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
-##################### Hard Starting Project ######################
+import random
+import smtplib
+import pandas as pd
+import datetime
 
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes. 
 
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Only the month and day matter. 
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
+my_email="amadornatalyyoseth@gmail.com"
+my_password="XXX"
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
+global letter_choose, new_letter, email
 
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+
+
+##################### Extra Hard Starting Project ######################
+
+today=datetime.datetime.now()
+list=["letter_1.txt","letter_2.txt","letter_3.txt"]
+letter_choose= random.choice(list)
+
+##read the csv
+birthdays_csv= pd.read_csv("birthdays.csv")
+
+#new_dict={new_key: newvalue for (index,data_row) in data.iterrows}
+new_dict={(row["month"],row["day"]):(row["name"],row["email"],row["year"],row["month"],row["day"] )for (index,row) in birthdays_csv.iterrows()}
+
+for key, value in new_dict.items():
+    month,day=key
+    name,email,year,_,_= value
+
+    if(month==today.month and day==today.day):
+        with open(f"letter_templates/{letter_choose}") as file:
+            letter_file = file.read()
+            new_letter=str(letter_file).replace("[NAME]",name)
+            email=email
+    break
+
+
+with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+    connection.starttls()
+    connection.login(user=my_email,password=my_password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs=email,
+                        msg=f"Subject:Happy Birthday\n\n{new_letter}")
 
 
 
